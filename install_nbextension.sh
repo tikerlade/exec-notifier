1cce44ce30388b7bf3067eb4a861ca6d91c0d6e8
Ivan Kuznetsov <tikerlade@gmail.com>
2021-04-04 22:32:26 +0300

    Update version of pypi package.

--- install_nbextension.sh
+++ install_nbextension.sh
@@ -5,3 +5,4 @@ mkdir -p $(jupyter --data-dir)/nbextensions/exec_notifier
 curl -LJO https://github.com/tikerlade/exec-notifier/tree/master/nbextension/exec_notifier.js > $(jupyter --data-dir)/nbextensions/exec_notifier/exec_notifier.js
 curl -LJO https://github.com/tikerlade/exec-notifier/tree/master/nbextension/exec_notifier.yaml > $(jupyter --data-dir)/nbextensions/exec_notifier/exec_notifier.yaml
 curl -LJO https://github.com/tikerlade/exec-notifier/tree/master/nbextension/README.md > $(jupyter --data-dir)/nbextensions/exec_notifier/README.md
+curl -LJO https://github.com/tikerlade/exec-notifier/tree/master/nbextension/bot_report.png > $(jupyter --data-dir)/nbextensions/exec_notifier/bot_report.png
